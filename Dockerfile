fb68abb7a0b360c3a18693734d47a395dfee64ff
soloboy <matolababa197@gmail.com>
2026-06-08 16:04:21 -0700

    add docker deployment

---
+++ Dockerfile
@@ -0,0 +1,12 @@
+FROM python:3.11
+
+WORKDIR /app
+
+COPY . /app
+
+RUN pip install --upgrade pip
+RUN pip install -r requirements.txt
+
+RUN python manage.py collectstatic --noinput
+
+CMD gunicorn safari_ya_maisha.wsgi:application --bind 0.0.0.0:10000
--- safari_ya_maisha/settings.py
+++ safari_ya_maisha/settings.py
@@ -12,10 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
-import platform
 
-if platform.python_version().startswith("3.14"):
-    raise Exception("Wrong Python version detected")
 from django.utils.translation import gettext_lazy as _
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
