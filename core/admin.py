4c488506b5cf2017cde7fd5256a450e2e2bfae79
soloboy <matolababa197@gmail.com>
2026-06-08 15:41:43 -0700

    set python version for render

--- core/admin.py
+++ core/admin.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
 from .models import Post, Comment
 
-
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
     list_display = ('title', 'category', 'created_at')
