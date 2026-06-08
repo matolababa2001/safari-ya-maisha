3f690e5b3de45d64c66c8b1fd60f26cd87436d0c
soloboy <matolababa197@gmail.com>
2026-06-08 13:05:40 -0700

    fix admin dicts error

--- core/admin.py
+++ core/admin.py
@@ -1,10 +1,14 @@
 from django.contrib import admin
 from .models import Post, Comment
 
+
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
     list_display = ('title', 'category', 'created_at')
+    search_fields = ('title', 'content')
+    list_filter = ('category',)
+
 
 @admin.register(Comment)
 class CommentAdmin(admin.ModelAdmin):
-    list_display = ('name', 'post', 'created_at')
+    list_display = ('id', 'created_at')
